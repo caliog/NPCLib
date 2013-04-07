dad4d273f8c49ef0fc75fa632f52a19eff0ece77
caliog <caliog@sharesc.com>
2013-04-07 14:19:41 +0300

    Create NPCNetworkManager.java

---
+++ com/sharesc/caliog/npclib/NPCNetworkManager.java
@@ -0,0 +1,50 @@
+package com.sharesc.caliog.npclib;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import net.minecraft.server.v1_5_R2.Connection;
+import net.minecraft.server.v1_5_R2.MinecraftServer;
+import net.minecraft.server.v1_5_R2.NetworkManager;
+import net.minecraft.server.v1_5_R2.Packet;
+
+/**
+ * 
+ * @author martin
+ */
+public class NPCNetworkManager extends NetworkManager {
+
+  public NPCNetworkManager() throws IOException {
+		super(MinecraftServer.getServer().getLogger(), new NullSocket(),
+				"NPC Manager", new Connection() {
+					@Override
+					public boolean a() {
+						return true;
+					}
+				}, null);
+		try {
+			Field f = NetworkManager.class.getDeclaredField("n");
+			f.setAccessible(true);
+			f.set(this, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void a(Connection nethandler) {
+	}
+
+	@Override
+	public void queue(Packet packet) {
+	}
+
+	@Override
+	public void a(String s, Object... aobject) {
+	}
+
+	@Override
+	public void a() {
+	}
+
+}
