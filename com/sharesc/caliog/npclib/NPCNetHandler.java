79d9c4ec8f5e3623b3441e59de13cc1e1c328935
caliog <caliog@sharesc.com>
2013-07-15 20:02:33 +0200

    Updated imports and vars

--- com/sharesc/caliog/npclib/NPCNetHandler.java
+++ com/sharesc/caliog/npclib/NPCNetHandler.java
@@ -1,28 +1,26 @@
 package com.sharesc.caliog.npclib;
 
-import net.minecraft.server.v1_5_R3.EntityPlayer;
-import net.minecraft.server.v1_5_R3.Packet;
-import net.minecraft.server.v1_5_R3.Packet101CloseWindow;
-import net.minecraft.server.v1_5_R3.Packet102WindowClick;
-import net.minecraft.server.v1_5_R3.Packet106Transaction;
-import net.minecraft.server.v1_5_R3.Packet10Flying;
-import net.minecraft.server.v1_5_R3.Packet130UpdateSign;
-import net.minecraft.server.v1_5_R3.Packet14BlockDig;
-import net.minecraft.server.v1_5_R3.Packet15Place;
-import net.minecraft.server.v1_5_R3.Packet16BlockItemSwitch;
-import net.minecraft.server.v1_5_R3.Packet18ArmAnimation;
-import net.minecraft.server.v1_5_R3.Packet19EntityAction;
-import net.minecraft.server.v1_5_R3.Packet255KickDisconnect;
-import net.minecraft.server.v1_5_R3.Packet3Chat;
-import net.minecraft.server.v1_5_R3.Packet7UseEntity;
-import net.minecraft.server.v1_5_R3.Packet9Respawn;
-import net.minecraft.server.v1_5_R3.PlayerConnection;
+import net.minecraft.server.v1_6_R2.EntityPlayer;
+import net.minecraft.server.v1_6_R2.Packet;
+import net.minecraft.server.v1_6_R2.Packet101CloseWindow;
+import net.minecraft.server.v1_6_R2.Packet102WindowClick;
+import net.minecraft.server.v1_6_R2.Packet106Transaction;
+import net.minecraft.server.v1_6_R2.Packet10Flying;
+import net.minecraft.server.v1_6_R2.Packet130UpdateSign;
+import net.minecraft.server.v1_6_R2.Packet14BlockDig;
+import net.minecraft.server.v1_6_R2.Packet15Place;
+import net.minecraft.server.v1_6_R2.Packet16BlockItemSwitch;
+import net.minecraft.server.v1_6_R2.Packet18ArmAnimation;
+import net.minecraft.server.v1_6_R2.Packet19EntityAction;
+import net.minecraft.server.v1_6_R2.Packet255KickDisconnect;
+import net.minecraft.server.v1_6_R2.Packet3Chat;
+import net.minecraft.server.v1_6_R2.Packet7UseEntity;
+import net.minecraft.server.v1_6_R2.Packet9Respawn;
+import net.minecraft.server.v1_6_R2.PlayerConnection;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_5_R3.CraftServer;
-import org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer;
-
-//import net.minecraft.server.v1_4_R1.MinecraftServer;
+import org.bukkit.craftbukkit.v1_6_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_6_R2.entity.CraftPlayer;
 
 /**
  * 
@@ -45,7 +43,7 @@ public class NPCNetHandler extends PlayerConnection {
 	}
 
 	@Override
-	public void d() {
+	public void e() {
 	};
 
 	@Override
