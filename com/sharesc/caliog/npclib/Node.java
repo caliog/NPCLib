9926f2d058484be296b658d1b2bca736416dead8
caliog <caliog@sharesc.com>
2014-03-03 16:01:05 +0100

    Update 1.7.2

--- com/sharesc/caliog/npclib/Node.java
+++ com/sharesc/caliog/npclib/Node.java
@@ -3,53 +3,52 @@ package com.sharesc.caliog.npclib;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.server.v1_6_R2.AxisAlignedBB;
+import net.minecraft.server.v1_7_R1.AxisAlignedBB;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_6_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R1.CraftWorld;
 
 public class Node { // Holds data about each block we check
 
-  static List<Material> liquids = new ArrayList<Material>();
+    static List<Material> liquids = new ArrayList<>();
 
-	static {
-		liquids.add(Material.WATER);
-		liquids.add(Material.STATIONARY_WATER);
-		// liquids.add(Material.LAVA); Maybe swimming in lava isn't the best
-		// idea for npcs
-		// liquids.add(Material.STATIONARY_LAVA);
-		liquids.add(Material.LADDER); // Trust me it makes sense
-	}
+    static {
+	liquids.add(Material.WATER);
+	liquids.add(Material.STATIONARY_WATER);
+	// liquids.add(Material.LAVA); Maybe swimming in lava isn't the best
+	// idea for npcs
+	// liquids.add(Material.STATIONARY_LAVA);
+	liquids.add(Material.LADDER); // Trust me it makes sense
+    }
 
-	int f, g = 0, h;
-	int xPos, yPos, zPos;
-	Node parent;
-	public Block b;
-	boolean notsolid, liquid;
+    int f, g = 0, h;
+    int xPos, yPos, zPos;
+    Node parent;
+    public Block b;
+    boolean notsolid, liquid;
 
-	public Node(final Block b) {
-		this.b = b;
-		xPos = b.getX();
-		yPos = b.getY();
-		zPos = b.getZ();
-		update();
-	}
+    public Node(Block b) {
+	this.b = b;
+	xPos = b.getX();
+	yPos = b.getY();
+	zPos = b.getZ();
+	update();
+    }
 
-	public void update() {
-		notsolid = true;
-		if (b.getType() != Material.AIR) {
-			AxisAlignedBB box = net.minecraft.server.v1_6_R2.Block.byId[
-				(new BWorld(b.getWorld())).
-			               getCraftWorld().
-			               getBlockTypeIdAt(b.getLocation())
-			           ].b(((CraftWorld) b.getWorld()).getHandle(), b.getX(), b.getY(), b.getZ());
-		if (box != null) {
-			if (Math.abs(box.e - box.b) > 0.2) {
-				notsolid = false;
-			}
+    @SuppressWarnings("deprecation")
+    public void update() {
+	notsolid = true;
+	if (b.getType() != Material.AIR) {
+	    final AxisAlignedBB box = net.minecraft.server.v1_7_R1.Block.e(b.getTypeId()).a(
+		    ((CraftWorld) b.getWorld()).getHandle(), b.getX(), b.getY(), b.getZ());
+	    if (box != null) {
+		if (Math.abs(box.e - box.b) > 0.2) {
+		    notsolid = false;
 		}
-		liquid = liquids.contains(b.getType());
+	    }
 	}
+	liquid = liquids.contains(b.getType());
+    }
 
 }
