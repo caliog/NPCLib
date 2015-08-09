b9f7cae2e1e3185022eaefa88305b9249df752c0
caliog <caliog@infinitras.net>
2015-08-09 20:16:56 +0200

    Update v1_8_R3

--- com/sharesc/caliog/npclib/NPCChannel.java
+++ com/sharesc/caliog/npclib/NPCChannel.java
@@ -1,66 +1,75 @@
-package com.sharesc.caliog.npclib;
+package org.caliog.npclib;
+
+import io.netty.channel.AbstractChannel;
+import io.netty.channel.ChannelConfig;
+import io.netty.channel.ChannelMetadata;
+import io.netty.channel.ChannelOutboundBuffer;
+import io.netty.channel.EventLoop;
 
 import java.net.SocketAddress;
 
-import net.minecraft.util.io.netty.channel.AbstractChannel;
-import net.minecraft.util.io.netty.channel.Channel;
-import net.minecraft.util.io.netty.channel.ChannelConfig;
-import net.minecraft.util.io.netty.channel.ChannelMetadata;
-import net.minecraft.util.io.netty.channel.ChannelOutboundBuffer;
-import net.minecraft.util.io.netty.channel.DefaultChannelConfig;
-import net.minecraft.util.io.netty.channel.EventLoop;
-
 public class NPCChannel extends AbstractChannel {
-    private final ChannelConfig config = new DefaultChannelConfig(this);
 
-    protected NPCChannel(Channel parent) {
-	super(parent);
+    public NPCChannel() {
+	super(null);
     }
 
+    @Override
     public ChannelConfig config() {
-	this.config.setAutoRead(true);
-	return this.config;
+	return null;
     }
 
-    protected void doBeginRead() throws Exception {
-    }
-
-    protected void doBind(SocketAddress arg0) throws Exception {
-    }
-
-    protected void doClose() throws Exception {
-    }
-
-    protected void doDisconnect() throws Exception {
-    }
-
-    protected void doWrite(ChannelOutboundBuffer arg0) throws Exception {
-    }
-
+    @Override
     public boolean isActive() {
 	return false;
     }
 
-    protected boolean isCompatible(EventLoop arg0) {
-	return true;
-    }
-
+    @Override
     public boolean isOpen() {
 	return false;
     }
 
-    protected SocketAddress localAddress0() {
-	return null;
-    }
-
+    @Override
     public ChannelMetadata metadata() {
 	return null;
     }
 
-    protected AbstractChannel.AbstractUnsafe newUnsafe() {
+    @Override
+    protected void doBeginRead() throws Exception {
+    }
+
+    @Override
+    protected void doBind(SocketAddress arg0) throws Exception {
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+    }
+
+    @Override
+    protected void doDisconnect() throws Exception {
+    }
+
+    @Override
+    protected void doWrite(ChannelOutboundBuffer arg0) throws Exception {
+    }
+
+    @Override
+    protected boolean isCompatible(EventLoop arg0) {
+	return false;
+    }
+
+    @Override
+    protected SocketAddress localAddress0() {
 	return null;
     }
 
+    @Override
+    protected AbstractUnsafe newUnsafe() {
+	return null;
+    }
+
+    @Override
     protected SocketAddress remoteAddress0() {
 	return null;
     }
